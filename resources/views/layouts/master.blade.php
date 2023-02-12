da1fcad92fea9b012f7c64393467ec4426558f2d
alfiSS59 <aalfi893@gmail.com>
2023-02-13 04:37:26 +0700

    mengubah icon & name

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -166,7 +166,7 @@
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
       <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Web Cetak</span>
     </a>
 
     <!-- Sidebar -->
@@ -174,10 +174,10 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="users">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">AlfiProject</a>
         </div>
       </div>
 
@@ -223,7 +223,7 @@
           </li>
           <li class="nav-item">
             <a href="../widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+              <i class="nav-icon fas fa-users"></i>
               <p>
                 profile percetakan
                 <span class="right badge badge-danger">New</span>
